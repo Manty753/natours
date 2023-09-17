988dcf242d9ce2f036a6964c19f8b0432d3b4036
Jakub Novák <kubanovak1561995@gmail.com>
2023-09-17 15:46:49 +0200

    Update https testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,16 +12,16 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSandToken = (user, statusCode, res) => {
+const createSandToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
   user.password = undefined;
 
   res.status(statusCode).json({
@@ -51,7 +51,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   //*3) if ok send token to client
-  createSandToken(user, 200, res);
+  createSandToken(user, 200, req, res);
 });
 exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
@@ -183,7 +183,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   await user.save();
   // 3) update changedPasswordAt => userModel.js
   //4) Log user send token
-  createSandToken(user, 200, res);
+  createSandToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -199,5 +199,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   //4/ log user in with new pass
-  createSandToken(user, 200, res);
+  createSandToken(user, 200, req, res);
 });
