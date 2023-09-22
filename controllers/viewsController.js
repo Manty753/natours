e14a9b40f322dd0898ad863aae6481ee082caebf
Jakub Novák <kubanovak1561995@gmail.com>
2023-09-22 08:42:44 +0200

    add stripe alerts

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,14 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.local.alert =
+      'Your booking was successful. If your booking doesnt show up immediately please come back later';
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   //1 get tour data from collection
   const tours = await Tour.find();
--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,8 @@ import { displayMap } from './leaflet';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
+
 // DOM ELEMENTS
 let leaflet = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -64,3 +66,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
